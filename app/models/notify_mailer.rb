ad8d16909c1cf6b547ba1a09b911904586039d47
Drew <drew@weblogicnow.com>
2011-04-19 20:04:46 -0400

    fixed the name in the email subject line

--- app/models/notify_mailer.rb
+++ app/models/notify_mailer.rb
@@ -2,7 +2,7 @@ class NotifyMailer < ActionMailer::Base
   def added_to_mem_notify(user,memory,name)
     recipients    user.email
     from          "notification@thememorygrid.com"
-    subject       "#{name} added you to a memory"
+    subject       "#{name.name} added you to a memory"
     body          :user => user, 
                   :memory => memory, 
                   :name => name

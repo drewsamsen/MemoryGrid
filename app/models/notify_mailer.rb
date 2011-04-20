8befff011d722f02a739846947c8c1c106eb737c
Drew <drew@weblogicnow.com>
2011-04-20 16:43:38 -0400

    modify *from* with the name memorygrid in mailer

--- app/models/notify_mailer.rb
+++ app/models/notify_mailer.rb
@@ -1,7 +1,7 @@
 class NotifyMailer < ActionMailer::Base
   def added_to_mem_notify(user,memory,name)
     recipients    user.email
-    from          "notification@thememorygrid.com"
+    from          "MemoryGrid <notification@thememorygrid.com>"
     subject       "#{name.name} added you to a memory"
     body          :user => user, 
                   :memory => memory, 
@@ -10,7 +10,7 @@ class NotifyMailer < ActionMailer::Base
 
   def comment_notify(current_user,comment,tagged_user)
     recipients    tagged_user.email
-    from          "notification@thememorygrid.com"
+    from          "MemoryGrid <notification@thememorygrid.com>"
     subject       "#{current_user.name} commented on your memory"
     body          :current_user => current_user, 
                   :comment => comment, 
